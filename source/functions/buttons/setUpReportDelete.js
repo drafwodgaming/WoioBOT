acc7ad30a71832752a6f52f4978e5602eab85d3e
Mark <drafwodgaming@yandex.ru>
2023-10-05 21:22:17 +0300

    refactor: add file setUpReportDelete.js

---
+++ source/functions/buttons/setUpReportDelete.js
@@ -0,0 +1,13 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+
+function reportDelete(reportDeleteId) {
+  const button = new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+      .setCustomId(reportDeleteId)
+      .setLabel("Delete")
+      .setStyle(ButtonStyle.Danger)
+  );
+  return button;
+}
+
+module.exports = { reportDelete };
