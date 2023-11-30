546f925bc8da748b758cb1d46bb776d64ced3240
Mark <drafwodgaming@yandex.ru>
2023-11-30 17:30:28 +0300

    update: function createVoiceChannel.js

--- source/functions/Utils/createVoiceChannel.js
+++ source/functions/Utils/createVoiceChannel.js
@@ -5,40 +5,40 @@ async function createVoiceChannel(
 	guild,
 	member,
 	channelName,
-	channelSize,
+	channelSize = 0,
 	parent
 ) {
+	const memberPermissions = [
+		PermissionFlagsBits.ManageChannels,
+		PermissionFlagsBits.MoveMembers,
+		PermissionFlagsBits.MuteMembers,
+		PermissionFlagsBits.Connect,
+		PermissionFlagsBits.KickMembers,
+		PermissionFlagsBits.Speak,
+		PermissionFlagsBits.SendMessages,
+		PermissionFlagsBits.Stream,
+	];
+
+	const guildPermissions = [
+		PermissionFlagsBits.Speak,
+		PermissionFlagsBits.SendMessages,
+		PermissionFlagsBits.Connect,
+		PermissionFlagsBits.ReadMessageHistory,
+		PermissionFlagsBits.ViewChannel,
+		PermissionFlagsBits.UseVAD,
+	];
+
+	const permissionOverwrites = [
+		{ id: member.id, allow: memberPermissions },
+		{ id: member.guild.id, allow: guildPermissions },
+	];
+
 	return guild.channels.create({
 		name: channelName,
 		type: ChannelType.GuildVoice,
-		parent: parent,
-		userLimit: channelSize || 0,
-		permissionOverwrites: [
-			{
-				id: member.id,
-				allow: [
-					PermissionFlagsBits.ManageChannels,
-					PermissionFlagsBits.MoveMembers,
-					PermissionFlagsBits.MuteMembers,
-					PermissionFlagsBits.Connect,
-					PermissionFlagsBits.KickMembers,
-					PermissionFlagsBits.Speak,
-					PermissionFlagsBits.SendMessages,
-					PermissionFlagsBits.Stream,
-				],
-			},
-			{
-				id: member.guild.id,
-				allow: [
-					PermissionFlagsBits.Speak,
-					PermissionFlagsBits.SendMessages,
-					PermissionFlagsBits.Connect,
-					PermissionFlagsBits.ReadMessageHistory,
-					PermissionFlagsBits.ViewChannel,
-					PermissionFlagsBits.UseVAD,
-				],
-			},
-		],
+		parent,
+		userLimit: channelSize,
+		permissionOverwrites,
 	});
 }
 
