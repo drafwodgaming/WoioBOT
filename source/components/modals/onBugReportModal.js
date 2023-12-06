9c27bae92b19091a433549d5d0d08fb019a1d84b
Mark <drafwodgaming@yandex.ru>
2023-12-06 21:52:21 +0300

    update: function onBugReportModal.js

--- source/components/modals/onBugReportModal.js
+++ source/components/modals/onBugReportModal.js
@@ -1,16 +1,24 @@
-const { sendBugReport } = require('@functions/sendBugReport');
+const { reportToDB } = require('@functions/utils/bugReport/reportToDB');
 const { modals } = require('@config/componentsId.json');
+const { sendBugReport } = require('@functions/utils/bugReport/sendBugReport');
 
 module.exports = {
 	data: {
 		name: modals.bugReport,
 	},
 	async execute(interaction, client) {
+		const userId = interaction.user.id;
 		const bugCommand = interaction.fields.getTextInputValue(modals.bugCommand);
 		const bugDescription = interaction.fields.getTextInputValue(
 			modals.bugDescription
 		);
 
-		await sendBugReport(interaction, client, bugCommand, bugDescription);
+		const { reportId } = await reportToDB(
+			interaction,
+			bugCommand,
+			bugDescription
+		);
+
+		await sendBugReport(client, userId, reportId, bugCommand, bugDescription);
 	},
 };
