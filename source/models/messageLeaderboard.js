558e7dc7f64fa937308fa85488eec8250780ad22
Mark <drafwodgaming@yandex.ru>
2024-01-01 08:22:22 +0300

    delete: messageLeaderboard from bot

--- source/models/messageLeaderboard.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-let messageLeaderboardSchema = new Schema({
-	guildId: String,
-	userId: String,
-	messageCount: Number,
-});
-
-module.exports = model('messageLeaderboard', messageLeaderboardSchema);
