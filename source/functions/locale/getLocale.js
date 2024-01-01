64a03ebdb6e4dbe07072ac1573a8099f80dae2f4
Mark <drafwodgaming@yandex.ru>
2024-01-01 08:23:51 +0300

    add: function getLocale.js

---
+++ source/functions/locale/getLocale.js
@@ -0,0 +1,11 @@
+const { getGuildLanguage } = require('@functions/utils/getGuildLanguage');
+const serverLocaleSchema = require('@source/models/serverLocale');
+
+async function getLocalizedText(interaction) {
+	if (interaction.guild) guildId = interaction.guild.id;
+	// Если взаимодействие происходит в личных сообщениях
+	else guildId = interaction.user.id;
+	const userLanguageCode = await getGuildLanguage(guildId, serverLocaleSchema);
+	return interaction.client.languages.get(userLanguageCode);
+}
+module.exports = { getLocalizedText };
