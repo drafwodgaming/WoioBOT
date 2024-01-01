e8cbbfff8d0378c85d5f81febb048080c26e9ae6
Mark <drafwodgaming@yandex.ru>
2024-01-01 08:23:27 +0300

    delete: past function local - userLocale.js

--- source/functions/locale/userLocale.js
+++
@@ -1,9 +0,0 @@
-const i18n = require('i18n');
-
-function getUserLocale(interaction) {
-	const userLocale = interaction?.locale || i18n.getLocale();
-	i18n.setLocale(userLocale);
-	return userLocale;
-}
-
-module.exports = { getUserLocale };
